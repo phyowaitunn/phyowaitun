dd75d1e7c594b920d5e1807f2446a5afaa58311c
phyowaitunn <pj7srsjb22@privaterelay.appleid.com>
2026-03-10 08:28:37 +0800

    Create script.js

---
+++ portfolio/script.js
@@ -0,0 +1,11 @@
+document.querySelectorAll("a").forEach(link=>{
+link.addEventListener("click",e=>{
+e.preventDefault()
+
+document.querySelector(link.getAttribute("href"))
+.scrollIntoView({
+behavior:"smooth"
+})
+
+})
+})
